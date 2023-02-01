d48bf0efe2502436c59fd8eb5bbc7f27d4066069
viktoria <viiikiii.g@gmail.com>
2023-02-01 18:25:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[scanner.nextInt()]; //задаем длинну массива
+        int number = scanner.nextInt(); // вводим число
+
+        for (int i = 0; i < a.length; i++) { //заполняем масив числами с консоли
+            a[i] = scanner.nextInt();
+
+        }
+
+        for (int i = 0; i < a.length; i++) { // перебираем каждый наш элемент массива
+            if (a[i] == number) { // находим тот что нам нужен и определяем его индекс
+                System.out.println(a[i]);// вывожим на экран индекс
+                break;
+
+            }
+            
+
+        }
+
+    }
+}
