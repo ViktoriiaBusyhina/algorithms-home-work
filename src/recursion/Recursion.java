8fad0357357b98a70f8d68e68cec4ac6ee5d67b0
viktoria <viiikiii.g@gmail.com>
2023-02-17 10:27:58 +0100

    д\

---
+++ src/recursion/Recursion.java
@@ -0,0 +1,56 @@
+package recursion;
+
+import static recursion.Recursion.sumOfDigits;
+
+public class Recursion {
+    public static int powerOfTwo(double n) {
+        if (n == 1) {
+            return 1;
+        }
+        else if (n < 1) {
+            return 0;
+        }
+        else {
+            return powerOfTwo(n / 2);
+        }
+
+}
+
+    public static int sumOfDigits(int n)
+    {
+        if (n < 10) {
+            return n;
+        }
+        else {
+            return (n % 10) + sumOfDigits(n / 10);
+        }
+    }
+
+}
+
+
+class HomeworkRecursionMain {
+    public static void main(String[] args) {
+
+        double n = 8192;
+
+        if (Recursion.powerOfTwo(n) == 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+        int sum = sumOfDigits((int) n);
+        System.out.println(sum);
+
+    }
+
+
+
+
+
+
+
+
+}
